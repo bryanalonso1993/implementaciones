e10a9c24c79483589d097003420008eb0112ac85
bryan alonso almeyda contreras <bryanalonsoalmeydacontreras@MacBook-Pro-de-bryan.local>
2025-04-13 09:36:01 -0500

    anotaciones deployment

---
+++ apuntes/notas.sh
@@ -0,0 +1,17 @@
+# anotar los cambios de un deployments
+kubectl annotate deployment -l app=airflow kubernetes.io/change-cause="Cambio global en Airflow" --overwrite -n platform-airflow-prod
+
+# EJM
+kubectl annotate deployment -l app=airflow kubernetes.io/change-cause="Actualización imagen Airflow v1" --overwrite
+
+# CMD
+MacBook-Pro-de-bryan:airflow bryanalonsoalmeydacontreras$ kubectl annotate deployment -l app=airflow kubernetes.io/change-cause="Actualización imagen Airflow v1" --overwrite
+deployment.apps/airflow-scheduler annotated
+deployment.apps/airflow-webserver annotated
+deployment.apps/airflow-worker annotated
+
+# Excelente
+MacBook-Pro-de-bryan:airflow bryanalonsoalmeydacontreras$ kubectl rollout history deployment airflow-scheduler
+deployment.apps/airflow-scheduler 
+REVISION  CHANGE-CAUSE
+1         Actualización imagen Airflow v1
